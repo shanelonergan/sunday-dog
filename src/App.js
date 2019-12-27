3e01e3d40b54ddbb67e90a992b55f5acde7f8847
shanelonergan <sptlonergan@gmail.com>
2019-12-27 17:28:38 -0500

    added grommet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grommet, ResponsiveContext } from 'grommet';
+import { grommet, dark } from 'grommet/themes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Grommet>
+                    <ResponsiveContext.Consumer>
+                        {size => (
+                            <Box fill background='brand'>
+                                <Heading>{`Sunday Dog`}</Heading>
+                            </Box>
+                        )}
+                    </ResponsiveContext.Consumer>
+                </Grommet>
+            </header>
+        </div>
+    );
 }
 
 export default App;
