0cc4f169bd3186dd09dd1f9a16e8c963e946f036
shanelonergan <sptlonergan@gmail.com>
2019-12-27 17:49:15 -0500

    changed header

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
                 <Grommet>
                     <ResponsiveContext.Consumer>
                         {size => (
-                            <Box width='100vw' height='100vh' background="url(https://cdn.royalcanin-weshare-online.io/GvqPH2YBIYfdNSoCoAgj/v1/bd18bl-bernese-mountain-dog-adult-standing)">
-                                <Heading>{`Sunday Dog`}</Heading>
+                            <Box overflow='hidden' justify='center' width='100vw' height='100vh' background="url(https://cdn.royalcanin-weshare-online.io/GvqPH2YBIYfdNSoCoAgj/v1/bd18bl-bernese-mountain-dog-adult-standing)">
+                                <Heading alignSelf='center' color='white'>{'Sunday Dog'}</Heading>
                             </Box>
                         )}
                     </ResponsiveContext.Consumer>
